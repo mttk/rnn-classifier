70c0b495f641614f182a3c78a6843c8362d7dfc7
IngTian <tzy20001018@gmail.com>
2021-12-17 14:30:03 -0500

    style: formatted datasets.py

--- datasets.py
+++ datasets.py
@@ -3,47 +3,48 @@ from torchtext.legacy import datasets
 
 
 def make_sst(batch_size, device=-1, fine_grained=False, vectors=None):
-    TEXT = data.Field(include_lengths=True, lower=True)
-    LABEL = data.LabelField()
-    filter_pred = lambda ex: ex.label != 'neutral' if not fine_grained else lambda ex: True
-    train, val, test = datasets.SST.splits(TEXT, LABEL,
-                                           fine_grained=fine_grained,
-                                           train_subtrees=False,
-                                           filter_pred=filter_pred
-                                           )
+    text_field = data.Field(include_lengths=True, lower=True)
+    label_field = data.LabelField()
+    f = lambda ex: ex.label != 'neutral' if not fine_grained else lambda ex: True
+    train, val, test = datasets.SST.splits(
+        text_field, label_field,
+        fine_grained=fine_grained,
+        train_subtrees=False,
+        filter_pred=f
+    )
 
-    TEXT.build_vocab(train, test, val, vectors=vectors)
-    LABEL.build_vocab(train, test, val)
+    text_field.build_vocab(train, test, val, vectors=vectors)
+    label_field.build_vocab(train, test, val)
     train_iter, val_iter, test_iter = data.BucketIterator.splits(
         (train, val, test), batch_size=batch_size, device=device, repeat=False)
 
-    return (train_iter, val_iter, test_iter), TEXT, LABEL
+    return (train_iter, val_iter, test_iter), text_field, label_field
 
 
 def make_imdb(batch_size, device=-1, vectors=None):
-    TEXT = data.Field(include_lengths=True, lower=True)
-    LABEL = data.LabelField()
-    train, test = datasets.IMDB.splits(TEXT, LABEL)
+    text_field = data.Field(include_lengths=True, lower=True)
+    label_field = data.LabelField()
+    train, test = datasets.IMDB.splits(text_field, label_field)
 
-    TEXT.build_vocab(train, test, vectors=vectors, max_size=30000)
-    LABEL.build_vocab(train, test)
+    text_field.build_vocab(train, test, vectors=vectors, max_size=30000)
+    label_field.build_vocab(train, test)
     train_iter, test_iter = data.BucketIterator.splits(
         (train, test), batch_size=batch_size, device=device, repeat=False)
 
-    return (train_iter, test_iter), TEXT, LABEL
+    return (train_iter, test_iter), text_field, label_field
 
 
 def make_trec(batch_size, device=-1, vectors=None):
-    TEXT = data.Field(include_lengths=True, lower=True)
-    LABEL = data.LabelField()
-    train, test = datasets.TREC.splits(TEXT, LABEL)
+    text_field = data.Field(include_lengths=True, lower=True)
+    label_field = data.LabelField()
+    train, test = datasets.TREC.splits(text_field, label_field)
 
-    TEXT.build_vocab(train, test, val, vectors=vectors)
-    LABEL.build_vocab(train, test, val)
+    text_field.build_vocab(train, test, vectors=vectors)
+    label_field.build_vocab(train, test)
     train_iter, test_iter = data.BucketIterator.splits(
         (train, test), batch_size=batch_size, device=device, repeat=False)
 
-    return (train_iter, test_iter), TEXT, LABEL
+    return (train_iter, test_iter), text_field, label_field
 
 
 dataset_map = {
@@ -51,16 +52,3 @@ dataset_map = {
     'IMDB': make_imdb,
     'TREC': make_trec
 }
-
-if __name__ == '__main__':
-    (tr, val, te), T, L = make_sst(20)
-    print("[SST] vocab: {} labels: {}".format(len(T.vocab), len(L.vocab)))
-    print("[SST] train: {} val: {} test {}".format(len(tr.dataset), len(val.dataset), len(te.dataset)))
-
-    (tr, te), T, L = make_imdb(20)
-    print("[IMDB] vocab: {} labels: {}".format(len(T.vocab), len(L.vocab)))
-    print("[IMDB] train: {} test {}".format(len(tr.dataset), len(te.dataset)))
-
-    (tr, te), T, L = make_trec(20)
-    print("[TREC] vocab: {} labels: {}".format(len(T.vocab), len(L.vocab)))
-    print("[TREC] train: {} test {}".format(len(tr.dataset), len(te.dataset)))
